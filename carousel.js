afc84f92197128fa0c68979790b3a8673b4c29f3
Pius-Muthomi <muthomipius001@gmail.com>
2025-06-20 17:37:07 +0300

    Initial commit with portfolio files

---
+++ carousel.js
@@ -0,0 +1,67 @@
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
+let carousel = document.querySelector('.carousel');
+let listItem = document.querySelector('.carousel .list');
+let thumbnail = document.querySelector('.carousel .thumbnail');
+
+let timeRunning = 4000;      // Time for animation to complete
+let timeAutoNext = 7000;     // Time between auto slides
+let runTimeOut;
+let runAutoRun;
+
+// Manual navigation
+nextBtn.onclick = function () {
+  showSlider('next');
+};
+
+prevBtn.onclick = function () {
+  showSlider('prev');
+};
+
+// Main slider function
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll('.carousel .list .item');
+  let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+  if (type === 'next') {
+    listItem.appendChild(itemSlider[0]);
+    thumbnail.appendChild(itemThumbnail[0]);
+    carousel.classList.add('next');
+  } else {
+    let last = itemSlider.length - 1;
+    listItem.prepend(itemSlider[last]);
+    thumbnail.prepend(itemThumbnail[last]);
+    carousel.classList.add('prev');
+  }
+
+  // Remove animation classes after it's done
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carousel.classList.remove('next');
+    carousel.classList.remove('prev');
+  }, timeRunning);
+
+  // Restart auto slide timer
+  clearTimeout(runAutoRun);
+  startAutoSlide();
+}
+
+// Auto slide function
+function startAutoSlide() {
+  runAutoRun = setTimeout(() => {
+    nextBtn.click();
+  }, timeAutoNext);
+}
+
+// Start auto sliding on page load
+startAutoSlide();
+
+// Autoload to stop on mouseover
+carousel.addEventListener("mouseover", () => {
+  clearTimeout(runAutoRun);
+});
+
+carousel.addEventListener("mouseout", () => {
+  startAutoSlide();
+});
+
---
+++ script.js
@@ -0,0 +1,116 @@
+const menuOpenButton = document.getElementById("menu-open-button");
+const menuCloseButton = document.getElementById("menu-close-button");
+
+// Toggle mobile menu visibility
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+// close menu on close-button
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click())
+
+
+// TAB CONTENT ON THE ABOUT SECTION
+
+const  tabLinks = document.getElementsByClassName("tab-links");
+const  tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    
+}
+
+
+// CV BUTTON
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'MUTHOMI_PIUS_PROFFESIONAL_CURRICULUM_VITAE.pdf';
+    link.download = 'MUTHOMI_PIUS_PROFFESIONAL_CURRICULUM_VITAE.pdf';
+    link.click();
+
+}
+
+// SUBMIT TO GOOGLE FORM
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbycTRcaOnUXiFlUqfkzx2T-_VI9wXCd416Z9nc7wfpJor8KTdCBiJC39VXjBmrARpHxmw/exec'
+//   const form = document.forms['submit-to-google-sheet']
+
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//       .then(response => {
+//         msg.innerHTML = "Message sent successfully!";
+    
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         }, 1000)
+
+//         form.reset()
+//       }
+//             )
+//       .catch(error => console.error('Error!', error.message))
+//   })
+
+//   SUBMITTED TO GOOGLE FORM
+
+// COPYRIGHT BUTTON
+
+const year = new Date().getFullYear();
+
+document.getElementById("copyright").innerHTML = `&copy; ${year} Muthomi. All rights reserved`
+
+// Sliding Carousel
+// $('.project-wrapper').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+
+
+// SLIDING CAROUSEL
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  // autoplay: true,
+  autoplaySpeed: 3000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
