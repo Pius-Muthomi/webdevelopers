4b95b61cf61e0add85fa8b567cd86eb7421b7920
Pius-Muthomi <muthomipius001@gmail.com>
2025-06-26 15:57:10 +0300

    Update changes made

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ document.getElementById("copyright").innerHTML = `&copy; ${year} Muthomi. All ri
 $('.post-wrapper').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  // autoplay: true,
+  autoplay: true,
   autoplaySpeed: 3000,
   nextArrow: $('.next'),
   prevArrow: $('.prev'),
