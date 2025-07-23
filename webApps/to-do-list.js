9e7d2eb34ae3e082d1a1e46e88c08d98d6a7b606
Pius-Muthomi <muthomipius001@gmail.com>
2025-07-23 08:32:45 +0300

    Push

--- webApps/to-do-list.js
+++ webApps/to-do-list.js
@@ -61,8 +61,14 @@ const updateStats = () => {
   const progressBar = document.getElementById('progress');
   progressBar.style.width =`${progress}%`;
 
-  document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`
-}
+  document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`;
+
+  if (tasks.length && completeTasks === totalTasks){
+    blaskConfetti();
+  }
+};
+
+
 
   const updateTaskList = () => {
     taskList.innerHTML = '';
@@ -99,4 +105,44 @@ const updateStats = () => {
     addTask();
   });
 
+const blaskConfetti = () => {
+  const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
 
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
