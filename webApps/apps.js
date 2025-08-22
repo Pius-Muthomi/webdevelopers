c2e2a180319e9760b99c3523e546e98181021809
Pius-Muthomi <muthomipius001@gmail.com>
2025-08-22 12:59:36 +0300

    pUSH

--- webApps/apps.js
+++ webApps/apps.js
@@ -25,4 +25,57 @@ function convertTemp() {
 
 }
 
-// TEMPERATURE COVERTER// TEMPERATURE COVERTER// TEMPERATURE COVERTER
+// BMI CALCULATOR // BMI CALCULATOR// BMI CALCULATOR
+let heightInput = document.getElementById('height');
+let weightInput  = document.getElementById('weight');
+let BMIresult = document.getElementById('BMIresult');
+const resetBtn = document.getElementById('resetBtn');
+const calcBtn = document.getElementById('calcBtn');
+let BMI; 
+
+function calcBMI(){
+    
+    height = parseFloat(heightInput.value)/100;
+    
+    weight = parseFloat(weightInput.value);
+    
+
+    if(!height || !weight || height <= 0 || weight <= 0){
+        window.alert= 'Please enter a valid height and weight';
+        
+    }
+    else{
+        BMI = weight/(height*height);
+        BMIresult.textContent = `BMI = ${BMI.toFixed(1)}`
+    };
+    // CALCULATE BMI
+    
+    
+}
+
+
+[height, weight].forEach(el => el.addEventListener('keydown', function(e){
+    if(e.key === 'Enter'){
+        calcBMI();
+    }
+}))
+
+resetBtn.addEventListener('click', (e)=> {
+    heightInput.value = "";
+    weightInput.value = "";
+    BMIresult.textContent = "Enter weight & height to calculate your BMI";
+    setTimeout(() =>{
+        BMIresult.textContent = ''
+    }, 3000)
+    
+})
+
+
+
+
+
+
+
+
+
+// BMI CALCULATOR
