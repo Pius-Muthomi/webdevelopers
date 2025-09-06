33b02ce2306024ae8636a67f53b1cc0d57f667e8
Pius-Muthomi <muthomipius001@gmail.com>
2025-09-06 16:48:07 +0300

    push

--- webApps/clock.js
+++ webApps/clock.js
@@ -31,3 +31,62 @@ function updateDate () {
 }
 
 updateDate();
+
+
+// STOPWATCH JS SCRIPT
+const display = document.getElementById('display');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+
+let startTime = 0;
+let elapsedTime = 0;
+let timeInterval;
+
+function startTimer (){
+    startTime = Date.now() - elapsedTime;
+
+  
+
+    timeInterval = setInterval( ()=>{
+          elapsedTime = Date.now() - startTime;
+          display.textContent = formatTime(elapsedTime);
+    } ,10)
+    
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+
+function formatTime(elapsedTime){
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    const seconds = Math.floor((elapsedTime % (1000 *60)) / 1000);
+    const minutes = Math.floor(elapsedTime % (1000 * 60 * 60) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+
+    return(
+        (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + 
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")  + ":" + 
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00")  + ":" + 
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds)
+    )
+}
+
+startBtn.addEventListener('click', startTimer);
+
+stopBtn.addEventListener('click', ()=>{
+    clearInterval(timeInterval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
+
+resetBtn.addEventListener('click', ()=>{
+    elapsedTime = 0
+    display.textContent = "00:00:00:00";
+    startBtn.disabled = false;
+    stopBtn.disabled = false;
+})
+
+
