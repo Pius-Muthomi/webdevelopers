11c890062dc4081df33c49aa6e246ad8e1ea4ee8
Pius-Muthomi <muthomipius001@gmail.com>
2025-09-06 15:56:13 +0300

    push

--- webApps/clock.js
+++ webApps/clock.js
@@ -1,5 +1,5 @@
 const clockDisplay = document.getElementById('clock-display');
-
+const dateDisplay = document.getElementById('date-display');
 function updateTime (){
     const time = new Date();
     const hours = time.getHours().toString().padStart(2, 0);
@@ -17,3 +17,17 @@ function updateTime (){
 }
 
 setInterval(updateTime, 1000);
+
+function updateDate () {
+    const date = new Date();
+    // const year = date.getFullYear();
+    // const month = date.getMonth() + 1;
+    // const day = date.getDate().toString().padStart(2, 0);
+
+    const options = {
+        day: "numeric", month:"long", year:"numeric"
+    };
+    dateDisplay.textContent = date.toLocaleDateString("en-GB", options)
+}
+
+updateDate();
